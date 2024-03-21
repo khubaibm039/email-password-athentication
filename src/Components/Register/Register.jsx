d1eabcc5110fb0f5667263b5b0f74f80ca21eea9
MOHAMMAD KHUBAIB SARKER <mohammad@kali.kali>
2024-03-22 02:26:58 +0600

    reguler expression validation and toggle show password

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,21 +1,26 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import auth from "../../Firebase/Firebase.config";
 import { useState } from "react";
+import { IoMdEyeOff, IoIosEye } from "react-icons/io";
 
 const Register = () => {
     const [registerError, setRegisterError] = useState("");
     const [success, setSuccess] = useState("");
+    const [isShow, setIsShow] = useState(false);
 
     const handleRegister = (e) => {
         e.preventDefault(); // page won't reload
 
         const email = e.target.email.value;
         const password = e.target.password.value;
-        console.log(email, password);
-
         if (password.length < 6) {
             setRegisterError(
                 "Password should be at least 6 characters or more"
+            );
+            return;
+        } else if (!/[A-Z]/.test(password)) {
+            setRegisterError(
+                "your password should have at least one upper case characters."
             );
             return;
         }
@@ -50,16 +55,25 @@ const Register = () => {
                         required
                     />
                     <br />
-                    <input
-                        className="mb-4 w-3/4 py-2 px-4 rounded-lg"
-                        type="password"
-                        name="password"
-                        placeholder="enter your password"
-                        required
-                    />
+                    <div>
+                        <input
+                            className="w-3/4 mb-4 py-2 px-4 rounded-lg  "
+                            type={isShow ? "text" : "password"}
+                            name="password"
+                            placeholder="enter your password"
+                            required
+                        />
+                        <span
+                            className="cursor-pointer text-2xl"
+                            onClick={() => setIsShow(!isShow)}
+                        >
+                            {isShow ? <IoIosEye /> : <IoMdEyeOff />}
+                        </span>
+                    </div>
+
                     <br />
                     <input
-                        className="btn btn-primary mb-4 w-3/4"
+                        className="btn btn-primary my-4 w-3/4"
                         type="submit"
                         name="register"
                     />
